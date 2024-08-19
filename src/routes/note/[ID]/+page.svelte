626a8d17b9140cc4869ebac2986baa1eac11ca0c
TuyuriTio <tuyuritio@gmail.com>
2024-08-19 13:57:30 +0800

    ADD: 笔记属性显示

--- src/routes/note/[ID]/+page.svelte
+++ src/routes/note/[ID]/+page.svelte
@@ -18,20 +18,38 @@
 		}
 
 		aside {
+			display: flex;
+			flex-direction: column;
+
 			flex-shrink: 0;
 			max-width: 250px;
 
 			margin-left: 20px;
 			padding-right: 10px;
 
-			overflow-y: auto;
-
 			h3 {
+				margin: 15px 0px;
+			}
+
+			dl {
+				display: flex;
+				align-items: center;
+
 				margin-bottom: 10px;
+
+				dt {
+					margin-right: 10px;
+				}
+
+				dd {
+					font-size: 0.8em;
+					line-height: normal;
+				}
 			}
 
 			q {
 				quotes: none;
+				overflow-y: auto;
 			}
 		}
 	}
@@ -49,12 +67,33 @@
 
 <main>
 	<div><Markdown>{@html data.content}</Markdown></div>
-	{#if data.contents}
-		<aside>
+	<aside>
+		<h3>属性</h3>
+
+		<dl>
+			<dt><Icon name="calendar" /></dt>
+			<dd>{Time(data.timestamp)}</dd>
+		</dl>
+
+		{#if data.series}
+			<dl>
+				<dt><Icon name="repo" /></dt>
+				<dd>{data.series}</dd>
+			</dl>
+		{/if}
+
+		{#if data.tags.length}
+			<dl>
+				<dt><Icon name="tag" /></dt>
+				<dd>{data.tags.join("; ")}</dd>
+			</dl>
+		{/if}
+
+		{#if data.contents}
 			<h3>目次</h3>
 			<q>{@html data.contents}</q>
-		</aside>
-	{/if}
+		{/if}
+	</aside>
 </main>
 
 <svelte:head>
@@ -62,7 +101,9 @@
 </svelte:head>
 
 <script lang="ts">
+	import Icon from "$lib/icon.svelte";
 	import Markdown from "$lib/markdown/markdown.svelte";
+	import Time from "$lib/time";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
